a1f6e98eb1ffb4b90fddb06e8d1abc6cc3c6108e
Lorenzo Fundaró García <lorenzo@ac.labf.usb.ve>
2010-04-28 21:34:29 -0430

    - Se agregan modelos en Sala, hace falta cambiar el tipo de dato   a algunos campos de las tablas por uno mas apropiado. - Se agregan modelos en Universidad, hay que revisar los tipos de datos - Se necesita saber cuales son las tablas a los que apuntan los FK   en el modelo de Sala. - Revisar que la concatenacion de string en la funcion unicode no sea   problematica

--- salas/models.py
+++ salas/models.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Solicitud_Software(models.Model):
+    """Contiene información sobre la solicitud 
+    de un software en particular"""
+    reservador = models.ForeignKey(???)
+    materia = models.ForeignKey(???)
+    nombre = models.CharField()
+    sistema_op = models.CharField()
+    licencia = models.CharField()
+    website = models.CharField()
+    version = models.CharField()
+    descripcion = models.CharField()
+    status = models.CharField()
+    direccion_ip = models.CharField()
--- universidad/models.py
+++ universidad/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Entidad(models.Model):
+    """Representa un departamento u otra institución
+    de la Universidad"""
+    codigo = models.CharField()
+    nombre = models.CharField()
+    direccion = models.CharField()
+
+    def __unicode__(self):
+        return self.nombre
+
+class Materia(models.Model):
+    """Identifica una materia"""
+    codigo = models.CharField(max_length = 6, primary_key = True)
+    nombre = models.CharField()
+    departamento = models.ForeignKey(Entidad)
+
+    def __unicode__(self): 
+        return self.nombre
+
+class Profesor(models.Model):
+    """Contiene los datos de un profesor registrado en el 
+    sistema"""
+    cedula = models.CharField(max_length = 9, primary_key = True)
+    nombre = models.CharField()
+    telefono = models.CharField()
+    email = models.CharField()
+    oficina = models.CharField()
+    departamento = models.ForeignKey(Entidad)
+    
+    def __unicode__(self):
+        return self.nombre + '/' + self.departamento
+
+class Encargado(models.Model):
+    """Contiene la información sobre el encargado de un curso.
+    Esta tabla surge de la necesidad de diferenciar las personas 
+    que son encargadas de un curso (estrictamente profesores) y 
+    las personas que dictan el curso (preparadores, profesores, 
+    ayudantes académicos, etc)."""
+    profesor = models.ForeignKey(Profesor)
+    departamento = models.ForeignKey(Entidad)
+
+    def __unicode__(self):
+        return self.profesor
+
